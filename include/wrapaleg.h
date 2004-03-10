c6ed2a57a7426d3c9aa121a1e51d446d0b827e71
Charles Bilyue <ckbilyue@gmail.com>
2004-03-10 01:36:45 +0000

    Updated copyright notices.

--- include/wrapaleg.h
+++ include/wrapaleg.h
@@ -3,8 +3,8 @@
 SNEeSe, an Open Source Super NES emulator.
 
 
-Copyright (c) 1998-2003 Charles Bilyue'.
-Portions Copyright (c) 2003 Daniel Horchner.
+Copyright (c) 1998-2004 Charles Bilyue'.
+Portions Copyright (c) 2003-2004 Daniel Horchner.
 
 This is free software.  See 'LICENSE' for details.
 You must read and accept the license prior to use.
--- makefile.dos
+++ makefile.dos
@@ -1,3 +1,17 @@
+#
+#
+# SNEeSe, an Open Source Super NES emulator.
+#
+#
+# Copyright (c) 1998-2004 Charles Bilyue'.
+# Portions Copyright (c) 2003-2004 Daniel Horchner.
+#
+# This is free software.  See 'LICENSE' for details.
+# You must read and accept the license prior to use.
+#
+#
+
+
 PLATFORM := dos
 
 # DOS (DJGPP, GCC)
