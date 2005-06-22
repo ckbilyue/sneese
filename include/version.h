b84c147c78dfb61fbaf0b819e7df7620518af0ef
Charles Bilyue <ckbilyue@gmail.com>
2005-06-22 10:18:53 +0000

    Preparation for release.

--- include/version.h
+++ include/version.h
@@ -26,6 +26,6 @@ You must read and accept the license prior to use.
 #define SNEESE_VERSION_STR "0.841"
 #define SNEESE_VERSION 0.841
 
-#define RELEASE_DATE __DATE__
+#define RELEASE_DATE "22 June 2005"
 
 #endif /* !defined(SNEeSe_version_h) */
